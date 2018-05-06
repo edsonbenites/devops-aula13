c69d824d8fe5857a69df397b9df18e0eb2ada2c7
Matheus Ferreira <36314624+matheustech@users.noreply.github.com>
2018-05-06 17:40:24 -0300

    Update Prova.sql

--- sql/Prova.sql
+++ sql/Prova.sql
@@ -1,4 +1,4 @@
 BEGIN;
-CREATE TABLE `app_prova` (`id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `nome_escola` varchar(200) NOT NULL, `endereco` varchar(50) NOT NULL, `cidade` varchar(200) NOT NULL, `estado` varchar(200) NOT NULL);
+CREATE TABLE `app_prova` (`id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY, `nome_escola` varchar(200) NOT NULL, `Endereco` varchar(50) NOT NULL, `cidade` varchar(200) NOT NULL, `estado` varchar(200) NOT NULL);
 
 COMMIT;
